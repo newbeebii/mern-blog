f0c3fdfe71ed91a96e18428bc2c90778a9121e8e
Alfred Sarunan <alfredsarunan01@gmail.com>
2025-07-05 11:06:45 +0800

    vidio ke 7 - belajar SCSS Global dan Slicing Halaman Login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { Button, Gap, Input, Link } from "../../components";
+import { LoginBg } from "../../assets";
 
 const Login = () => {
   return (
-    <div>
-      <p>Login Page</p>
+    <div className="main-page">
+      <div className="left">
+        <img src={LoginBg} className="bg-image" alt="imageBg" />
+      </div>
+      <div className="right">
+        <p className="title">Login</p>
+        <Input label="Email" placeholder="Email" />
+        <Gap height={18} />
+        <Input label="Password" placeholder="Password" />
+        <Gap height={20} />
+        <div className="button-wrapper">
+          <Button title="Login" />
+        </div>
+        <Gap height={130} />
+        <Link title="Sign Up ?" />
+      </div>
     </div>
   );
 };
