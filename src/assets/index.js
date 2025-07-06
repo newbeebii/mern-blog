20d68f55f4dc4e5b6553c70a8eb2ebbc15a422ea
Alfred Sarunan <alfredsarunan01@gmail.com>
2025-07-06 08:27:42 +0800

    vidio ke 9 membuat styling MainApp header dan Footer

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,23 @@
 import RegisterBg from "./image/registerBg.jpg";
 import LoginBg from "./image/loginBg.jpg";
+import LogoBlog from "./image/logo.png";
 
-export { RegisterBg, LoginBg };
+//icon
+import ICFacebook from "./icon/facebook.svg";
+import ICTwitter from "./icon/twitter.svg";
+import ICInstagram from "./icon/instagram.svg";
+import ICDiscord from "./icon/discord.svg";
+import ICGithub from "./icon/github.svg";
+import ICTelegram from "./icon/facebook.svg";
+
+export {
+  RegisterBg,
+  LoginBg,
+  LogoBlog,
+  ICFacebook,
+  ICDiscord,
+  ICGithub,
+  ICInstagram,
+  ICTelegram,
+  ICTwitter,
+};
--- src/pages/MainApp/index.js
+++ src/pages/MainApp/index.js
@@ -1,19 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import CreateBlog from "../CreateBlog";
 import DetailBlog from "../DetailBlog";
 import Home from "../Home";
+import { Footer, Header } from "../../components";
+import "./mainApp.scss";
 
 const MainApp = () => {
   return (
-    <div>
-      <p>Header</p>
-      <Routes>
-        <Route path={"create-blog"} element={<CreateBlog />}></Route>
-        <Route path="/" element={<Home />}></Route>
-        <Route path={"detail-blog"} element={<DetailBlog />}></Route>
-      </Routes>
-      <p>Footer</p>
+    <div className="main-app-wrapper">
+      <Header />
+      <div className="content-wrapper">
+        <Routes>
+          <Route path={"create-blog"} element={<CreateBlog />}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path={"detail-blog"} element={<DetailBlog />}></Route>
+        </Routes>
+      </div>
+      <Footer />
     </div>
   );
 };
