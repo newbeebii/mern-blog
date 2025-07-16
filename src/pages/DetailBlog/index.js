893a9da7ad80fa4595966d4d1f5acdbd1bd41e3c
Alfred Sarunan <alfredsarunan01@gmail.com>
2025-07-17 07:01:21 +0800

    vidio ke 12 - membuat styling halaman Detail Blog Post

--- src/pages/DetailBlog/index.js
+++ src/pages/DetailBlog/index.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { RegisterBg } from "../../assets";
+import "./detailBlog.scss";
 
 const DetailBlog = () => {
-  return <div>Detail Blog Page</div>;
+  return (
+    <div className="detail-blog-wrapper">
+      <img className="img-cover" src={RegisterBg} alt="" />
+      <p className="blog-title">Title Blog</p>
+      <p className="blog-author">Author - Date Post</p>
+      <p className="blog-body">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam vero
+        error, ea sapiente deleniti debitis laudantium iste eaque reiciendis
+        rerum sint in hic modi. Debitis quam molestias cupiditate earum
+        saepe?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores
+        ut repellat voluptates earum tempora modi facere cupiditate omnis non
+        nihil nemo ratione maxime dolor quasi, ipsam quaerat? Culpa, assumenda
+        dolorum!Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Dolores ut repellat voluptates earum tempora modi facere cupiditate
+        omnis non nihil nemo ratione maxime dolor quasi, ipsam quaerat? Culpa,
+        assumenda dolorum!
+      </p>
+    </div>
+  );
 };
 
 export default DetailBlog;
