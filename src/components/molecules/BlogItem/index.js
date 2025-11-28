37add17d6ae8c52ebdbbc9d15c6ae5ee7dfa15c5
Alfred Sarunan <alfredsarunan01@gmail.com>
2025-11-29 00:01:49 +0800

    43 - Mengganti data dummy di home page dengan response API

--- src/components/molecules/BlogItem/index.js
+++ src/components/molecules/BlogItem/index.js
@@ -4,20 +4,17 @@ import "./blogitem.scss";
 import { Button, Gap } from "../../atoms";
 import { useNavigate } from "react-router-dom";
 
-const BlogItem = () => {
+const BlogItem = (props) => {
   const navigate = useNavigate();
   return (
     <div className="blog-item">
-      <img className="image-thumb" src={RegisterBg} alt="post" />
+      <img className="image-thumb" src={props.image} alt="post" />
       <div className="content-detail">
-        <p className="title">Title Blog</p>
-        <p className="author">Author - Date Post</p>
-        <p className="body">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit
-          hic praesentium quas dolor omnis dicta? Soluta autem distinctio
-          assumenda. Officia asperiores ab suscipit. Tempora ad voluptates
-          voluptatum sit facere consectetur?
+        <p className="title">{props.title}</p>
+        <p className="author">
+          {props.name} - {props.date}
         </p>
+        <p className="body">{props.body}</p>
         <Gap height={20} />
         <Button title="Detail" onClick={() => navigate("/detail-blog")} />
       </div>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { BlogItem, Button, Gap } from "../../components";
 import "./home.scss";
 import { useNavigate } from "react-router";
 import axios from "axios";
 
 const Home = () => {
+  const [dataBlog, setDataBlog] = useState([]);
   useEffect(() => {
     axios
-      .get("http://localhost:4000/v1/blog/posts")
+      .get("http://localhost:4000/v1/blog/posts?page=2&perPage=2")
       .then((result) => {
         console.log(result.data);
+        const responseAPI = result.data;
+
+        setDataBlog(responseAPI.data);
       })
       .catch((err) => {
         console.log(err);
@@ -23,10 +27,18 @@ const Home = () => {
       </div>
       <Gap height={20} />
       <div className="content-wrapper">
-        <BlogItem />
-        <BlogItem />
-        <BlogItem />
-        <BlogItem />
+        {dataBlog.map((blog) => {
+          return (
+            <BlogItem
+              key={blog._id}
+              image={`http://localhost:4000/${blog.image}`}
+              title={blog.title}
+              body={blog.body}
+              name={blog.author.name}
+              date={blog.createdAt}
+            />
+          );
+        })}
       </div>
       <div className="pagination">
         <Button title="Previous" />
